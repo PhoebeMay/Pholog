63a5174c7fa1e2b5c86e42b2cb76f8809d9d31cf
Phoebe <phoebenichols@gmail.com>
2019-08-04 18:18:19 +0100

    remove build files

--- _build/default/src/code_generation/.formatted/genFunctionTable.mli
+++
@@ -1,8 +0,0 @@
-open ParseTree
-open Dt
-open Core
-
-val genFunctionTable :
-  var program ->
-  (functionId, (location clause * int) sexp_list) Base.Hashtbl.t
-  * (location clauseBodyVal sexp_list * int)
--- _build/default/src/endToEndTest.ml
+++
@@ -1,4 +0,0 @@
-open CombineTests
-open OUnit
-
-let _ = run_test_tt_main suite
